81b3523b869234431b046ecb77c77a0c733a1b25
Jules Yvon <jules.yvon@etu.utc.fr>
2022-04-28 10:06:13 +0000

    Update long_lat.py

--- long_lat.py
+++ long_lat.py
@@ -17,3 +17,22 @@ for i in range(len(country_name)):
 
 print(str(result_lat))
 print(str(result_long))
+
+
+
+for i in range(len(country_name)):
+	gold_count.append(0)
+	silver_count.append(0)
+	bronze_count.append(0)
+	total_count.append(0)
+	
+	for j in range(len(data_medal)):
+		if country_name[i] == data_country_name[j]:
+			if data_medal[j] == "Gold":
+				gold_count[i] += 1
+			elif data_medal[j] == "Silver":
+				silver_count[i] += 1
+			elif data_medal[j] == "Bronze":
+				bronze_count[i] += 1
+	total_count[i] = gold_count[i] * 3 + silver_count[i] * 2 + bronze_count[i]
+
